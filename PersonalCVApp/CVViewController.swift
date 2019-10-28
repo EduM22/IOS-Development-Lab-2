75d5ffbde307130e43d23077d970f6d964224adf
J <99johanbogg@gmail.com>
2019-10-28 21:19:30 +0100

    Created CV view + CVViewController

---
+++ PersonalCVApp/CVViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CVViewController.swift
+//  PersonalCVApp
+//
+//  Created by Johan Törnqvist on 2019-10-28.
+//  Copyright © 2019 Johan Törnqvist. All rights reserved.
+//
+
+import UIKit
+
+class CVViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
