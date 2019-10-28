6c6da57e9b18183969a965f751a19cf917c1ee06
J <99johanbogg@gmail.com>
2019-10-28 18:57:49 +0100

    Initial Commit

---
+++ PersonalCVApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalCVApp
+//
+//  Created by Johan Törnqvist on 2019-10-28.
+//  Copyright © 2019 Johan Törnqvist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
