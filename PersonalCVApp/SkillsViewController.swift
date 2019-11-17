12b9b38651ed7bf6a5c8d5b31cd869f989f0fc35
J <99johanbogg@gmail.com>
2019-11-17 10:30:20 +0100

    updated the animation

--- PersonalCVApp/SkillsViewController.swift
+++ PersonalCVApp/SkillsViewController.swift
@@ -39,32 +39,30 @@ class SkillsViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        UIView.animate(withDuration: 1.5) {
+        UIView.animate(withDuration: 2.5) {
             self.view.backgroundColor = .white
+        }
+        
+        UIView.animate(withDuration: 4.5) {
             self.swiftLabel.alpha = 1
             self.SQLLabel.alpha = 1
             self.pythonLabel.alpha = 1
             self.cplusplusLabel.alpha = 1
             self.cLabel.alpha = 1
             self.dartLabel.alpha = 1
+            
+            self.swiftLabel.textColor = .darkGray
+            self.SQLLabel.textColor = .blue
+            self.pythonLabel.textColor = .brown
+            self.cplusplusLabel.textColor = .cyan
+            self.cLabel.textColor = .magenta
+            self.dartLabel.textColor = .orange
         }
-        
     }
-    
 
     @IBAction func closeSkillsView(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
